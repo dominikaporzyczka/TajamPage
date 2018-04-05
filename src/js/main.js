281bb988c6fae683c04b531b743c2191f1becee8
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-04-05 22:38:36 +0200

    Add button to toggle menu for devices with width less than 815px

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 // Add class 'active' to active nav item
 const addActiveClassToNavElement = (function () {
     const navBarItem = document.querySelectorAll('.nav-bar li a');
-
+    const navBar = document.querySelector('.nav-bar');
+        
     for (let i = 0; i < navBarItem.length; i++) {
         navBarItem[i].addEventListener('click', function () {
             document.querySelector('.nav-bar .active').classList.remove('active');
             this.classList.add('active');
+            navBar.classList.remove('show-nav-bar');
         });
     }
 })();
@@ -18,10 +20,21 @@ const navScroll = (function () {
         console.log(document.body.scrollTop);
         const top = window.pageYOffset || document.documentElement.scrollTop;
         if (top > 100) {
-            navBarTop.style.background = 'linear-gradient(rgba(76, 61, 114, 0.7), rgba(76, 61, 114, 0.7))';
+            navBarTop.style.background = 'linear-gradient(rgba(76, 61, 114, 0.8), rgba(76, 61, 114, 0.8))';
         }
         else {
             navBarTop.style.background = 'transparent';
         }
+    });
+})();
+
+const toggleNav = (function() {
+    const navToggleIcon = document.querySelector('.nav_toggler');
+
+    navToggleIcon.addEventListener('click', function(e) {
+        const navBar = document.querySelector('.nav-bar');
+        navBar.classList.toggle('show-nav-bar');
+
+        e.preventDefault();
     });
 })();
