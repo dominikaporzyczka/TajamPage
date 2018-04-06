cc28798b926f17e723d27d7f7fc3c794f1701860
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-04-06 11:38:42 +0200

    Refactor in class names

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,27 @@
-(function() {
+(function () {
     const navBarTop = document.getElementById('nav-fixed'),
         navBar = document.querySelector('.nav-bar'),
         navBarItem = document.querySelectorAll('.nav-bar li a'),
         navToggleIcon = document.querySelector('.nav_toggler');
-        
+
     // Add class 'active' to active nav item
     for (let i = 0; i < navBarItem.length; i++) {
         navBarItem[i].addEventListener('click', function () {
-            document.querySelector('.nav-bar .active').classList.remove('active');
-            this.classList.add('active');
+            document.querySelector('.nav-bar .nav-bar_active').classList.remove('nav-bar_active');
+            this.classList.add('nav-bar_active');
             navBar.classList.remove('show-nav-bar');
         });
     }
 
     // Show nav bar after click toggler button
-    navToggleIcon.addEventListener('click', function(e) {
+    navToggleIcon.addEventListener('click', function (e) {
         navBar.classList.toggle('show-nav-bar');
-
+        
         e.preventDefault();
     });
 
     // Change color of nav's background during scrolling
     window.addEventListener('scroll', function () {
-        console.log(document.body.scrollTop);
         const top = window.pageYOffset || document.documentElement.scrollTop;
         if (top > 100) {
             navBarTop.style.background = 'linear-gradient(rgba(76, 61, 114, 0.8), rgba(76, 61, 114, 0.8))';
