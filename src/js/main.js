0ace26b2b3d299cfd1d678a01e60fe8b1d3be76b
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-04-10 22:51:52 +0200

    Add scroll animation after click nav bar item

--- src/js/main.js
+++ src/js/main.js
@@ -2,24 +2,62 @@
     const navBarTop = document.getElementById('nav-fixed'),
         navBar = document.querySelector('.nav-bar'),
         navBarItem = document.querySelectorAll('.nav-bar li a'),
-        navToggleIcon = document.querySelector('.nav_toggler');
-        teamMembers = document.querySelectorAll('.team-member');
-        peoplesPhotos = document.querySelectorAll('.peoplesay_content__imgs img');
+        navToggleIcon = document.querySelector('.nav_toggler'),
+        teamMembers = document.querySelectorAll('.team-member'),
+        peoplesPhotos = document.querySelectorAll('.peoplesay_content__imgs img'),
         peoplesOpinions = document.querySelectorAll('.peoplesay_content__opinion');
-    
+
+    /**
+     * @description Gets coordinates of element
+     * @param {Element} el - HTML element
+     */
+    function getOffset(el) {
+        el = el.getBoundingClientRect();
+        return {
+            left: el.left + window.scrollX,
+            top: el.top + window.scrollY
+        }
+    }
+
+    /**
+     * @description Scrolls to specific position
+     * @param {Element} element - reference element
+     * @param {*} to - scroll position
+     * @param {*} duration - duration of animation
+     */
+    function scrollTo(element, to, duration) {
+        if (duration <= 0) return;
+        var difference = to - element.scrollTop;
+        var perTick = difference / duration * 10;
+
+        setTimeout(function () {
+            element.scrollTop = element.scrollTop + perTick;
+            if (element.scrollTop == to) return;
+            scrollTo(element, to, duration - 10);
+        }, 10);
+    }
+
     // Add class 'active' to active nav item
     for (let i = 0; i < navBarItem.length; i++) {
-        navBarItem[i].addEventListener('click', function () {
+        navBarItem[i].addEventListener('click', function (e) {
             document.querySelector('.nav-bar .nav-bar_active').classList.remove('nav-bar_active');
             this.classList.add('nav-bar_active');
             navBar.classList.remove('show-nav-bar');
+
+            const attribute = this.getAttribute('href'),
+                element = document.querySelector(attribute),
+                offset = getOffset(element);
+                        
+            scrollTo(document.documentElement, offset.top - 93, 500);
+
+            e.preventDefault();
         });
     }
 
     // Show nav bar after click toggler button
     navToggleIcon.addEventListener('click', function (e) {
         navBar.classList.toggle('show-nav-bar');
-        
+
         e.preventDefault();
     });
 
@@ -38,11 +76,11 @@
     for (let i = 0; i < teamMembers.length; i++) {
         const img = teamMembers[i].querySelector('img');
 
-        teamMembers[i].addEventListener('mouseenter', function() { 
+        teamMembers[i].addEventListener('mouseenter', function () {
             img.classList.add('flipInY');
         });
 
-        teamMembers[i].addEventListener('mouseleave', function() {
+        teamMembers[i].addEventListener('mouseleave', function () {
             img.classList.remove('flipInY');
         });
     }
@@ -51,7 +89,7 @@
     for (let i = 0; i < peoplesPhotos.length; i++) {
         const animation = 'fadeInRight';
 
-        peoplesPhotos[i].addEventListener('click', function() {
+        peoplesPhotos[i].addEventListener('click', function () {
             for (let j = 0; j < peoplesPhotos.length; j++) {
                 peoplesPhotos[j].classList.remove('active');
                 peoplesOpinions[j].classList.remove(animation);
@@ -63,5 +101,4 @@
             peoplesOpinions[i].classList.add('active');
         });
     }
-    
 })();
